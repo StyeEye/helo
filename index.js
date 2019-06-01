d50d49d75d20e82f8f70c9dc5eb016d827057b3a
StyeEye <ColonialBuff@protonmail.com>
2019-06-01 14:09:12 -0600

    Database setup

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const massive = require('massive');
+require('dotenv').config();
+
 const controller = require('./server/controller');
 
 const server = express();
 
 server.use(cors());
 server.use(bodyParser.json());
+
+massive(process.env.CONNECTION_STRING)
+    .then(db => {
+        server.set("db", db);
+        console.log(`Connected to database`);
+    });
 
 const port = process.env.PORT || 8040;
 
